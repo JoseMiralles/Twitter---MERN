77c5979d520fb00887fd17ef49c8e5e6a2c5f15d
Jose Miralles <josemiralles1991@gmail.com>
2021-01-30 18:21:45 -0500

    Implemented api routes for users and tweets.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// IMPORT ROUTES:
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 const app = express();
 const db = require("./config/keys").mongoURI;
@@ -7,7 +12,14 @@ mongoose.connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB!"))
     .catch(err => console.log(err));
 
+// MIDDLEWARE:
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ROUTES:
 app.get("/", (req, res) => res.send("Hello World, by Jose Miralles!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
